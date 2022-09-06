c1294116ce51cb4248c11cb3677ccbf5ee565ac4
taehyun9244 <namtaehyun9244@gmail.com>
2022-09-06 23:25:11 +0900

    SpringMVC 예외처리

---
+++ src/main/java/hello/excption/ExcptionApplication.java
@@ -0,0 +1,13 @@
+package hello.excption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/excption/ExcptionApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.excption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
